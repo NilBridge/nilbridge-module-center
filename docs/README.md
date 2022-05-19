6c49c3af47c2adadaf25b8d9867c3201643a8f42
Sbaoor <2959435045@qq.com>
2022-05-19 20:34:28 +0800

    update docs

--- docs/README.md
+++ docs/README.md
@@ -44,6 +44,28 @@
 
 # 下载模块
 
-有了上面的信息后，我们就可以进行对比和下载模块了
+有了上面的信息后，我们就可以进行下载模块了
 
-github.io在国内访问极慢，所以我们选用JSDelivr来进行加速
+在`build`项的`success`为`true`的前提下，访问`path`项
+
+比如上边`bilibilisearch`的`path`是`/1f64751306ed76c15995fbfc79fbf78d/bilibilisearch.nbpack`
+
+直接访问`http://modules.nilbridge.site/1f64751306ed76c15995fbfc79fbf78d/bilibilisearch.nbpack`就可以下载了
+
+但是`github.io`在国内的访问速度不尽人意，不过我们可以用jsdelivr提供的加速服务进行全球加速
+
+虽然jsdelivr的缓存很难绕过，但是我们可以用更新访问路径的方式来跳过缓存
+
+拼接url：`https://fastly.jsdelivr.net/gh/NilBridge/nilbridge-module-center@master/docs{path}`
+
+这里的`path`就是上面`build`中的`path`，直接访问就可以通过jsdelivr提供的高速cdn获取到文件
+
+# 解压模块
+
+模块下载下来之后是`.nbpack`格式，其实就是`.tar.gz`
+
+用`tgz`的方式进行解压就可以用了
+
+具体可以使用`compressing`模块来解压
+
+具体详见[npm](https://www.npmjs.com/package/compressing)
--- helper/tgz.py
+++ helper/tgz.py
@@ -1,4 +1,5 @@
 import os, tarfile
+import gzip
 
 #一次性打包整个根目录。空子目录会被打包。
 #如果只打包不压缩，将"w:gz"参数改为"w:"或"w"即可。
@@ -9,4 +10,29 @@ def make_targz(output_filename:str, source_dir:str,):
   如果只打包不压缩，将"w:gz"参数改为"w:"或"w"即可。'''
   with tarfile.open(output_filename,'w:gz') as tar:
     tar.add(source_dir, arcname=os.path.basename(source_dir))
+    tar.close()
+
+def un_gz(file_name):
+    """ungz zip file"""
+    f_name = file_name.replace(".gz", "")
+    #获取文件的名称，去掉
+    g_file = gzip.GzipFile(file_name)
+    #创建gzip对象
+    open(f_name, "wb+").write(g_file.read())
+    #gzip对象用read()打开后，写入open()建立的文件里。
+    g_file.close() #关闭gzip对象
+ 
+   
+
+def un_tar(file_name):
+    #untar zip file
+    tar = tarfile.open(file_name)
+    names = tar.getnames()
+    if os.path.isdir(file_name + "_files"):
+        pass
+    else:
+        os.mkdir(file_name + "_files")
+    #由于解压后是许多文件，预先建立同名文件夹
+    for name in names:
+        tar.extract(name, file_name + "_files/")
     tar.close()
