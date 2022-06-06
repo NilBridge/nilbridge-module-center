b3e0590ee4eafd89cc658eff02852287ae572395
Sbaoor <2959435045@qq.com>
2022-06-06 16:03:23 +0800

    周一 2022/06/06 16:03:23.57

--- helper/checkupdate.py
+++ helper/checkupdate.py
@@ -5,13 +5,13 @@ def github_update(repo_url:str):
     try:
         repo = repo_url.split('github.com/')[1]
         repo = repo.split('.git')[0]
-        r = requests.get(f'https://api.github.com/repos/{repo}',verify=False)
+        r = requests.get(headers={'Authorization':'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'},url=f'https://api.github.com/repos/{repo}',verify=False)
         print(f'get https://api.github.com/repos/{repo} with code:{r.status_code}')
         if(r.status_code != 200):
             return {'find':False,'code':r.status_code}
         info = json.loads(r.text)
         url = info['url']
-        release = requests.get(f'{url}/releases/latest',verify=False)
+        release = requests.get(headers={'Authorization':'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'},url=f'{url}/releases/latest',verify=False)
         if(release.status_code != 200):
             return {'find':False,'code':release.status_code}
         latest = json.loads(release.text)
