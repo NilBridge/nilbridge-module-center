f3038f904250b3a475dfc46eaa12ac235ff6d336
Sbaoor <2959435045@qq.com>
2022-06-10 09:11:42 +0800

    周五 2022/06/10  9:11:41.86

--- helper/checkupdate.py
+++ helper/checkupdate.py
@@ -15,7 +15,7 @@ def github_update(repo_url:str):
         if(release.status_code != 200):
             return {'find':False,'code':release.status_code}
         latest = json.loads(release.text)
-        return {'find':True,'tag_name':latest['tag_name'],'owner':info['owner']['login'],'repo_name':info['name']}
+        return {'find':True,'tag_name':latest['name'],'owner':info['owner']['login'],'repo_name':info['name']}
     except Exception as err:
         print(err)
         return {'find':False,'code':0}
